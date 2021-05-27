fd458d5d2e9b650503580f4ab5b0d4fea26a662b
manurubio1997 <manuelrubio97@gmail.com>
2021-05-27 17:49:19 +0200

    ModificacionScraperTripAd
    Añadido analisis de sentimiento al scraper (Falta por pefeccionar)

--- PC2-3/app/Models/Clima.php
+++ PC2-3/app/Models/Clima.php
@@ -8,16 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Clima extends Model
 {
     use HasFactory;
-    //REPASAR
-    $clima->Clima::'id';
-    $clima->'idMunicipio';
-    $clima->'Nombre';
-    $clima->'Fecha';
-    $clima->'tMaxima';
-    $clima->'tMainima';
-    $clima->'tMedia';
-    $clima->'Humedad';
-    $clima->'Presion';
-    $clima->'Viento';
 
 }
